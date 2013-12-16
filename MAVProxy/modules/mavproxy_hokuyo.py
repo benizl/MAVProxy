97f873b6cecd691e77a35c9ed3513855403d64c5
Ben Nizette <benjamin.nizette@anu.edu.au>
2013-12-16 14:35:46 +0800

    Add circle-fitting routines to the hokuyo module, allowing it to detect and control to poles by either geometric fitting or regression (the former is about 100x faster, the latter a bit more reliable)

--- MAVProxy/modules/mavproxy_hokuyo.py
+++ MAVProxy/modules/mavproxy_hokuyo.py
@@ -28,6 +28,22 @@ def idle_task():
 def unload():
 	mpstate.laser_ranger.unload()
 
+def dist(x1, y1, x2, y2):
+	from math import sqrt
+	return sqrt((x1 - x2)**2 + (y1 - y2)**2)
+
+def circle_func(ps, x, y, r):
+	''' Circle function. Take negative sqrt as the circle we're
+	    fitting is generated from LOS measurements in the positive
+	    y direction'''
+	ys = []
+	for p in ps:
+		rs = r**2 - (p - x)**2
+		if rs < 0:
+			rs = 0
+		ys.append(y - sqrt(rs))
+
+	return numpy.array(ys)
 
 def cmd_hokuyo(args):
 	if len(args) > 0:
@@ -63,9 +79,15 @@ class laser_ranger:
 		self.cluster_min_dist = 100 # 10cm
 		self.cluster_min_count = 10
 
-		self.laser_cluster = 2
+		self.laser_cluster = 1
+
+		self.find_circles = 1
+		self.use_regression = 0
 
 		self._terminate = False
+
+		self._circ_lr = 0
+		self._circ_lb = 0
 
 		self._laser_queue = multiprocessing.Queue()
 		self._laser_thread = threading.Thread(target=self.laser_thread)
@@ -94,6 +116,160 @@ class laser_ranger:
 			#print("Msg sent")
 			
 
+	def clustered_min(self, d):
+		min_cluster_dist = 5600
+		min_cluster_angle = 0
+		min_dist = 5600
+		min_angle = 0
+		cluster_count = 0
+		print("C MIN")
+		for i in range(self.min_laser_index // self.laser_cluster,
+		               self.max_laser_index // self.laser_cluster):
+			if abs(d[i] - d[i-1]) < self.cluster_thresh:
+				cluster_count += 1
+				if d[i] < min_cluster_dist and d[i] > self.cluster_min_dist:
+					min_cluster_dist = d[i]
+					min_cluster_angle = self._laser.index_to_radians(i)
+			elif cluster_count > self.cluster_min_count:
+				if min_cluster_dist < min_dist:
+					min_dist = min_cluster_dist
+					min_angle = min_cluster_angle
+				cluster_count = 0
+				min_cluster_dist = 5600
+				min_cluster_angle = 0
+			else:
+				cluster_count = 0
+				min_cluster_dist = 5600
+				min_cluster_angle = 0
+
+		# If we finished within a cluster we have to check minima here too
+		if min_cluster_dist < min_dist:
+			min_dist = min_cluster_dist
+			min_angle = min_cluster_angle
+
+		return (time.time(), min_dist, min_angle)
+
+
+	def find_circle_regressive(self, cd, cb):
+		#print("CIR REG")
+		circles = []
+		cx = [ r * sin(bear) for r, bear in zip(cd, cb)]
+		cy = [ r * cos(bear) for r, bear in zip(cd, cb)]
+
+		centx0 = (cx[0] + cx[-1]) / 2.0
+		centy0 = (cy[0] + cy[-1]) / 2.0
+
+		r0 = dist(cx[0], cx[-1], cy[0], cy[-1]) / 2.0
+
+		try:
+			popt, pcov = scipy.optimize.curve_fit(circle_func, cx, cy,
+				p0=(centx0, centy0, r0), xtol=0.001, ftol=0.001)
+		except RuntimeError:
+			return None #didn't converge
+
+		try:
+			popt[2] = abs(popt[2]) # Could have converge to -ve radius
+
+			# Accept circles with radius between 10cm and under 3 metres
+			# centroid variance better than 20cm and radial variance better
+			# than 20cm. These must be a little larger than you might expect
+			# do to point data corruption during rapid movement
+			if popt[2] > 50 and popt[2] < 3000 and pcov[0,0] < 200 and pcov[1,1] < 200 and pcov[2,2] < 200:
+				return popt
+		except TypeError:
+			pass # pcov is inf if the system didn't converge (and inf can't be subscripted)
+
+		return None
+
+	def find_circle_geometric(self, cd, cb):
+		from math import sqrt, sin, cos, pi
+		#print("CIR GEO")
+
+		# This algorithm assumes that the end points are tangent to the circle,
+		# but in fact it's very common for the absolute endpoints to be especially
+		# noisy.  As such we use the samples one in from each end to calculate the
+		# circle parameters; they're close enough to being tangent that it still works
+		a, b = cd[1], cd[-2]
+		ta, tb = cb[1], cb[-2]
+		theta = tb - ta
+
+		r = sqrt(a**2 + b**2 - 2 * a * b * cos(theta)) / 2
+		r = r / (cos(theta / 2))
+
+		xc = a * sin(ta) + r * sin(ta - pi/2)
+		yc = a * cos(ta) + r * cos(ta - pi/2)
+
+		xo = [ i * sin(j) for i, j in zip(cd, cb)]
+		yo = [ i * cos(j) for i, j in zip(cd, cb)]
+
+		ressqr = [(dist(xc, yc, x, y) - r)**2 for x, y in zip(xo, yo)]
+		score = sqrt(sum(ressqr) / len(ressqr))
+
+		# The score is the RMS residual to the circle in mm. The
+		# threshold is set empirically
+		if r > 50 and r < 3000 and score < 40:
+			return (xc, yc, r)
+		else:
+			return None
+
+	def clustered_circles(self, d, regressive=False):
+		from math import atan2
+		ranges, bearings = [],[]
+		circles = []
+		cluster_count = 0
+		for i in range(self.min_laser_index // self.laser_cluster,
+		               self.max_laser_index // self.laser_cluster):
+			if abs(d[i] - d[i-1]) < self.cluster_thresh and d[i] > 100:
+				cluster_count += 1
+				ranges.append(d[i])
+				bearings.append(-self._laser.index_to_radians(i))
+			elif cluster_count > self.cluster_min_count:
+				if regressive:
+					c = self.find_circle_regressive(ranges, bearings)
+				else:
+					c = self.find_circle_geometric(ranges, bearings)
+				if c is not None:
+					circles.append(c)
+				ranges, bearings = [], []
+				cluster_count = 0
+			else:
+				cluster_count = 0
+				ranges, bearings = [], []
+
+		# If we finished within a cluster we have to check minima here too
+		c = None
+		if cluster_count > self.cluster_min_count:
+			if regressive:
+				c = self.find_circle_regressive(ranges, bearings)
+			else:
+				c = self.find_circle_geometric(ranges, bearings)
+
+		if c is not None:
+			circles.append(c)
+
+		# If we didn't find any circles, return the last sample.  Not that
+		# nice, but hopefully doesn't happen much..
+		if len(circles) == 0:
+			return (time.time(), self._circ_lr, self._circ_lb)
+
+		# Bearing to centre of closest circle, range to the edge of that
+		# circle along that bearing
+		d_min = 5600
+		chosen = None
+		for c in circles:
+			d = dist(c[0], c[1], 0, 0)
+			if d < d_min:
+				d_min = d
+				chosen = c
+
+		bear = atan2(chosen[0], chosen[1])
+		rang = d_min - chosen[2]
+
+		self._circ_lr, self._circ_lb = rang, bear
+
+		return (time.time(), rang, bear)
+
+
 	def laser_thread(self):
 		''' Laser thread.
 		    Sets up scans, calculates range and bearing to object and
@@ -107,40 +283,19 @@ class laser_ranger:
 			cluster_count = 0
 			d = self._laser.read_scan()
 
+			#d = ([0] * 367) + [592, 574, 559, 554, 546, 540, 537, 533, 529, 527, 527, 524, 509, 508, 507, 502, 497, 497, 497, 495, 494, 495, 495, 494, 494, 493, 491, 491, 491, 491, 495, 495, 487, 493, 486, 486, 490, 493, 500, 508, 510, 511, 512, 513, 515, 523, 524, 531, 546, 546, 547, 552, 572, 573, 620] + ([0] * 347)
+
 			if d is None:
 			#	print("Scan failed")
 				continue
 			#else:
 			#	print("Scan")
 
-			min_cluster_dist = 5600
-			min_cluster_angle = 0
-
-			for i in range(self.min_laser_index // self.laser_cluster,
-			               self.max_laser_index // self.laser_cluster):
-				if abs(d[i] - d[i-1]) < self.cluster_thresh:
-					cluster_count += 1
-					if d[i] < min_cluster_dist and d[i] > self.cluster_min_dist:
-						min_cluster_dist = d[i]
-						min_cluster_angle = self._laser.index_to_radians(i)
-				elif cluster_count > self.cluster_min_count:
-					if min_cluster_dist < min_dist:
-						min_dist = min_cluster_dist
-						min_angle = min_cluster_angle
-					cluster_count = 0
-					min_cluster_dist = 5600
-					min_cluster_angle = 0
-				else:
-					cluster_count = 0
-					min_cluster_dist = 5600
-					min_cluster_angle = 0
-
-			# If we finished within a cluster we have to check minima here too
-			if min_cluster_dist < min_dist:
-				min_dist = min_cluster_dist
-				min_angle = min_cluster_angle
-
-			sample = (time.time(), min_dist, min_angle)
+			if self.find_circles:
+				sample = self.clustered_circles(d)
+			else:
+				sample = self.clustered_min(d)
+	
 
 			self._laser_queue.put(sample)
 
